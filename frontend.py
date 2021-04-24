09bbd97b8f6007e81a5c3ac94c7f9a81b792cfbf
cristina gradinaru <cristinagradinaru90@gmail.com>
2021-04-24 17:37:36 -0500

    executable file for bookstore app

--- frontend.py
+++ frontend.py
@@ -1,7 +1,44 @@
 import tkinter as tk
 import backend
 
+def get_selected_row(event):
+    global selected_row
+    if len(list1.curselection())> 0:
+        index=list1.curselection()[0]
+        selected_row=list1.get(index)
+        e1.delete(0,tk.END)
+        e1.insert(tk.END,selected_row[1])
+        e2.delete(0,tk.END)
+        e2.insert(tk.END,selected_row[2])
+        e3.delete(0,tk.END)
+        e3.insert(tk.END,selected_row[3])
+        e4.delete(0,tk.END)
+        e4.insert(tk.END,selected_row[4])
+        return selected_row
+
+def view_command():
+    list1.delete(0,tk.END)
+    for book in backend.view():
+        list1.insert(tk.END,book)
+
+def search_command():
+    list1.delete(0,tk.END)
+    for book in backend.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
+        list1.insert(tk.END,book)
+
+def insert_command():
+    backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    list1.delete(0,tk.END)
+    list1.insert(tk.END,(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
+
+def delete_command():
+    backend.delete(selected_row[0])
+
+def update_command():
+    backend.update(selected_row[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+
 window = tk.Tk()
+window.wm_title("Cristina's BookStore")
 
 l1=tk.Label(window, text='Title')
 l1.grid(row=0,column=0)
@@ -41,22 +78,24 @@ sb1.grid(row=2, column=2, rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1=tk.Button(window, text="View all", width=12)
+list1.bind('<<ListboxSelect>>', get_selected_row)
+
+b1=tk.Button(window, text="View all", width=12, command= view_command)
 b1.grid(row=2,column=3)
 
-b2=tk.Button(window, text="Search entry", width=12)
+b2=tk.Button(window, text="Search entry", width=12, command=search_command)
 b2.grid(row=3,column=3)
 
-b3=tk.Button(window, text="Add entry", width=12)
+b3=tk.Button(window, text="Add entry", width=12, command=insert_command)
 b3.grid(row=4,column=3)
 
-b4=tk.Button(window, text="Update", width=12)
+b4=tk.Button(window, text="Update", width=12, command=update_command)
 b4.grid(row=5,column=3)
 
-b5=tk.Button(window, text="Delete", width=12)
+b5=tk.Button(window, text="Delete", width=12, command=delete_command)
 b5.grid(row=6,column=3)
 
-b6=tk.Button(window, text="Close", width=12)
+b6=tk.Button(window, text="Close", width=12, command=window.destroy)
 b6.grid(row=7,column=3)
 
 window.mainloop()
